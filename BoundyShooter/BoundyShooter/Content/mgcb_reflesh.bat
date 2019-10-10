5195fc8be4b05838cc459e70f2545a52dd4762dd
RyoTakanashi <K019G1339@gmail.com>
2019-10-10 11:21:36 +0900

    Content.mgcb再作成用のバッチファイルの追加

---
+++ BoundyShooter/BoundyShooter/Content/mgcb_reflesh.bat
@@ -0,0 +1,26 @@
+﻿@echo off
+cd /d %~dp0
+
+echo Content.mgcbを再作成します
+
+setlocal
+
+echo #----------------------------- Global Properties ----------------------------# > Content.mgcb
+
+echo /outputDir:bin/$(Platform) >> Content.mgcb
+echo /intermediateDir:obj/$(Platform) >> Content.mgcb
+echo /platform:Windows >> Content.mgcb
+echo /config: >> Content.mgcb
+echo /profile:Reach >> Content.mgcb
+echo /compress:False >> Content.mgcb
+
+echo #-------------------------------- References --------------------------------# >> Content.mgcb
+
+
+echo #---------------------------------- Content ---------------------------------# >> Content.mgcb
+
+endlocal
+
+echo 再作成が完了しました
+
+exit /b 0
