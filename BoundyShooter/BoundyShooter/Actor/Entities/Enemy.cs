33013adf67161503befe444ff77154873c53f1c3
Hirakawa0404 <k019g1329.hirakawa.daiki@gmail.com>
2019-10-11 11:10:19 +0900

    敵が角度を計算して反射するように設定

--- BoundyShooter/BoundyShooter/Actor/Entities/Enemy.cs
+++ BoundyShooter/BoundyShooter/Actor/Entities/Enemy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BoundyShooter.Manager;
 using BoundyShooter.Util;
 using Microsoft.Xna.Framework;
 
@@ -13,34 +14,68 @@ namespace BoundyShooter.Actor.Entities
         private Vector2 bounceVelocity = new Vector2(0, -5f);//跳ね返りの初速
         private float acceleration = 0.1f;
         /// <summary>
-        /// 敵の最高速度
+        /// 敵のY軸方向の最高速度
         /// </summary>
-        public float MaxSpeed
+        public float MaxSpeedY
         {
             get;
             protected set;
         } = 3.0f;
+        /// <summary>
+        /// 敵のX軸方向の最高速度
+        /// </summary>
+        public float MaxSpeedX
+        {
+            get;
+            protected set;
+        } = 3.0f;
+
         public Enemy(string name, Vector2 position, Point size)
             : base(name, position, size)
         {
         }
 
+
         public override void Hit(GameObject gameObject)
         {
             if(gameObject is Player)
             {
-                Velocity = bounceVelocity;
+                float speed = 10f;
+                var playerPos = GameObjectManager.Instance.Find<Player>().First().Position;
+                //var playerSize = GameObjectManager.Instance.Find<Player>().First().Size;
+                var rotation = Math.Atan2(playerPos.Y - Position.Y, playerPos.X - Position.X);
+                if(rotation < 0)
+                {
+                    //var position = Position;
+                    //position.Y = playerPos.Y + playerSize.Y / 2;
+                    //Position = position;
+                    Velocity = new Vector2((float)Math.Cos(rotation), (float)Math.Sin(rotation)) * speed;
+                }
+                else
+                {
+                    Velocity = new Vector2((float)Math.Cos(rotation), -(float)Math.Sin(rotation)) * speed;
+                }
+                
+                //Velocity = bounceVelocity;
             }
             base.Hit(gameObject);
         }
 
         public override void Update(GameTime gameTime)
         {
-            if (Velocity.Y < MaxSpeed)
+            var velocity = Velocity;
+            if (Velocity.Y < MaxSpeedY)
             {
-                var velocity = Velocity;
                 velocity.Y += acceleration;
                 Velocity = velocity;
+            }
+            if (Velocity.Y > MaxSpeedY)
+            {
+                velocity.Y = MaxSpeedY;
+            }
+            if(Velocity.X > MaxSpeedX)
+            {
+                velocity.X = MaxSpeedX;
             }
             base.Update(gameTime);
         }
