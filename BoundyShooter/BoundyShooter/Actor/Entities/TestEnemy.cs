c0a869421a89ed4ec867110ca894455eee9c5194
RyoTakanashi <K019G1339@gmail.com>
2019-10-21 03:45:04 +0900

    エネミーにアニメーションを実装

--- BoundyShooter/BoundyShooter/Actor/Entities/TestEnemy.cs
+++ BoundyShooter/BoundyShooter/Actor/Entities/TestEnemy.cs
@@ -4,20 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BoundyShooter.Actor.Particles;
+using BoundyShooter.Util;
 using Microsoft.Xna.Framework;
 
 namespace BoundyShooter.Actor.Entities
 {
     class TestEnemy : Enemy
     {
+        private Animation animation;
+
         public TestEnemy(Vector2 position)
             : base("enemy1", position, new Point(64, 64))
         {
+            animation = new Animation(Size, 8, 0.1f);
         }
 
         public TestEnemy(TestEnemy other)
             :this(other.Position)
         { }
+
         public override object Clone()
         {
             return new TestEnemy(this);
@@ -26,6 +31,7 @@ namespace BoundyShooter.Actor.Entities
         public override void Update(GameTime gameTime)
         {
             if (!IsInScreen()) return;
+            animation.Update(gameTime);
             base.Update(gameTime);
         }
 
@@ -36,6 +42,14 @@ namespace BoundyShooter.Actor.Entities
             {
                 new DestroyParticle(Name, Position, Size, DestroyParticle.DestroyOption.Center);
             }
+        }
+
+        public override void Draw()
+        {
+            var drawer = Drawer.Default;
+            drawer.DisplayModify = true;
+            drawer.Rectangle = animation.GetRectangle();
+            base.Draw(drawer);
         }
     }
 }
