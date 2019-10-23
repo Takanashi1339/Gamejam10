50f3a6620cacf96b666a47ac8d67dd273e70a1b6
kazu1303 <k019g1239.uchida.kazuki@gmail.com>
2019-10-23 15:49:00 +0900

    タイトル画面にプレイヤーを描画

--- BoundyShooter/BoundyShooter/Scene/Title.cs
+++ BoundyShooter/BoundyShooter/Scene/Title.cs
@@ -7,23 +7,33 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using BoundyShooter.Device;
 using BoundyShooter.Util;
+using BoundyShooter.Def;
+using BoundyShooter.Actor.Particles;
+using BoundyShooter.Actor.Entities;
+using BoundyShooter.Manager;
+using BoundyShooter.Actor;
+using BoundyShooter.Actor.Blocks;
 
 namespace BoundyShooter.Scene
 {
     class Title : IScene
     {
         private bool isEndFlag;
-        private int titlenum;
-        private Timer titleTimer;
-
+        public static readonly int titleBottom = 235;
         private Renderer renderer;
+        private ParticleManager particleManager;
+        private Animation animation;
+
+        private Player player;
+
 
         public Title()
         {
             isEndFlag = false;
             renderer = Renderer.Instance;
-            titlenum = 0;
-            titleTimer = new Timer(0.15f, true);
+            particleManager = new ParticleManager();
+            player = new Player(new Vector2(Screen.Width / 2 - 32, Screen.Height / 2 - 32));
+            animation = new Animation(new Point(467,235), 4, 0.15f,Animation.AnimationType.Vertical);
         }
 
         public void Draw()
@@ -33,15 +43,18 @@ namespace BoundyShooter.Scene
             renderer.Begin();
 
             var titledrawer = new Drawer();
-            titledrawer.Rectangle = new Rectangle(0, 235 * titlenum, 467, 235);
+            titledrawer.Rectangle = animation.GetRectangle();
             renderer.DrawTexture("title", new Vector2(20, 0), titledrawer);
 
+            player.Draw();
+            particleManager.Draw();
             renderer.End();
         }
 
         public void Initialize()
         {
             isEndFlag = false;
+            particleManager.Initialize();
         }
 
         public bool IsEnd()
@@ -65,15 +78,11 @@ namespace BoundyShooter.Scene
                 //シーン移動
                 isEndFlag = true;
             }
+            animation.Update(gameTime);
+            particleManager.Update(gameTime);
             titleTimer.Update(gameTime);
-            if (titleTimer.IsTime)
-            {
-                titlenum += 1;
-            }
-            if(titlenum == 4)
-            {
-                titlenum = 0;
-            }
+            player.ModeTitle();
+            player.Update(gameTime);
         }
     }
 }
